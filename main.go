ade2f2ddebc4d61eaaec88a9974556b8c232ac39
liuyulvv <liuyulvv@outlook.com>
2023-12-03 15:59:46 +0800

    fix: cors

--- main.go
+++ main.go
@@ -2,20 +2,9 @@ package main
 
 import (
 	"WhoaServer/router"
-	"time"
-
-	"github.com/gin-contrib/cors"
 )
 
 func main() {
 	r := router.SetupRouter()
-	// r.Use(cors.Default())
-	r.Use(cors.New(cors.Config{
-		AllowAllOrigins:  true,
-		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
-		AllowHeaders:     []string{"Content-Type", "Authorization"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
 	r.Run(":8080")
 }
--- router/router.go
+++ router/router.go
@@ -3,11 +3,13 @@ package router
 import (
 	"WhoaServer/router/user"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(cors.Default())
 	user.SetupUserRouter(r)
 	return r
 }
